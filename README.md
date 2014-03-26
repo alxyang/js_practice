c8d951d1ffd8978960c331b26d5c8c61556f7115
Alex Yang <alyx.yang@gmail.com>
2014-03-26 03:19:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Javascript/Jquery Practice
+
+In this test file you will find example usage of
+- jquery deferred
+- module pattern
+- closure
+- IIFE (immediately invoked function expressions)
+- prototypal inheritance
+
+Server set up using NodeJS.
---
+++ index.js
@@ -0,0 +1,13 @@
+var server = require("./server"); //require the file and assign to variable
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle); //call function within that mfile
+
+
+
